421f96141647e9d08868115608db55469115554a
Yohei Yoshikawa <yoshikawa@telepath.co.jp>
2019-07-12 17:53:50 +0900

    under construction

--- app/settings/pgsql/debian8.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_NAME', 'sample');
-    define('DB_HOST', 'kona.telepath');
-    define('DB_USER', 'postgres');
-    define('DB_PORT', '5432');
-    //define('DB_PASS', PROJECT_MANAGER_DB_PASS);
-    //define('PG_INFO', 'host=localhost user=postgres dbname='.PROJECT_MANAGER_DB_NAME);
