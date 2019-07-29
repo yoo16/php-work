f4ecd8a619cd4fbaf4c20ffc1d08f1720aa88853
Yohei Yoshikawa <yoshikawa@telepath.co.jp>
2019-07-29 20:22:59 +0900

    under construction

---
+++ app/controllers/DocumentController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DocumentController 
+ *
+ * @author  Yohei Yoshikawa
+ * @create  2019/07/03 
+ */
+require_once 'AppController.php';
+
+class DocumentController extends AppController {
+
+	var $layout = 'sample';
+	var $name = 'document';
+
+    function index() {
+	}
+	
+}
